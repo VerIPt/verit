72cd959a5731774deeebaf23f4ad9ef9e7a47db1
s89799 <s89799>
2024-03-12 19:24:07 +0100

    home almost ready

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,44 @@
-import Logo from '../../assets/images/verit_logo.png'
-import './index.scss'
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
+import Logo from '../../assets/images/verit_logo.png';
+import P from '../../assets/images/p.png';
+import './index.scss';
 
 const Home = () => {
-    return(
-        <>
+    const el = useRef(null);
+    const typed = useRef(null);
+
+    useEffect(() => {
+        const options = {
+            strings: ['Software Engineer', 'Web Developer', 'Media-Designer'],
+            typeSpeed: 100,
+            backSpeed: 100,
+            backDelay: 1000,
+            loop: true,
+        };
+
+        // el.current referenziert das span-Element, wo der Effekt angezeigt wird
+        typed.current = new Typed(el.current, options);
+
+        return () => {
+            // Typed.js sauber destruieren
+            typed.current.destroy();
+        };
+    }, []);
+
+    return (
         <section className="home" id="home">
             <div className="home-content">
                 <h3>Hello, my Name is</h3>
-                <h1>Pius Maushart</h1>
-                <h3>And I'm a <span>Software Engineer</span></h3>
+                <h1><img className='p' src={P} alt=''></img>ius <span className='surname'>Maushart</span></h1>
+                <h3>And I'm a <span className='software' ref={el}></span></h3>
                 <p>I am Ver-IT services, at your service for IT</p>
-                
-                
             </div>
             <div className="home-img">
                 <img src={Logo} alt="logo"></img>
-            </div>
+            </div> 
         </section>
-        </>
     );
-}
+};
 
 export default Home;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,6 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/pius.png'
+import LogoS from '../../assets/images/Logo_ohneService.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
@@ -11,7 +11,7 @@ const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoS} alt="logo" />
-            <img className='sub-logo' src={LogoSubtitle} alt="subtitle" />
+            {/* <img className='sub-logo' src={LogoSubtitle} alt="subtitle" /> */}
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
