1dc90dce638c4c26621043893248508597266963
s89799 <s89799>
2024-03-13 12:25:59 +0100

    home ready

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -27,13 +27,15 @@ const Home = () => {
 
     return (
         <section className="home" id="home">
-            <div className='background'/>
+            
             <div className="home-content">
                 <h3>Hello, my Name is</h3>
                 <h1><img className='p' src={P} alt=''></img>ius <span className='surname'>Maushart</span></h1>
                 <h3>And I'm a <span className='software' ref={el}></span></h3>
                 <p>I am Ver-IT services, at your service for IT</p>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                <div className='background'/>
+                <div className='hoverbackground'/>
             </div>
             <div className="home-img">
                 <img src={Logo} alt="logo"></img>
