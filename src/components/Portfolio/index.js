85c3f7c4046a5df884204fd739fedd5abd5e1a54
s89799 <s89799>
2024-03-14 17:08:12 +0100

    skil ready

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,15 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import './index.scss'
-import { faAngular, faCss3, faHtml5, faJava, faJs, faPython, faReact } from '@fortawesome/free-brands-svg-icons';
 import html from '../../assets/images/logo1.png'
 import css from '../../assets/images/logo2.png'
 import cpp from '../../assets/images/cpp.png'
 import java from '../../assets/images/java.png'
 import react from '../../assets/images/react.png'
 import js from '../../assets/images/js.png'
+import raytracer from '../../assets/images/ray_tracer.png'
+import louhaus from '../../assets/images/louhaus.png'
+import treemap from '../../assets/images/treemap.png'
+
 
 const Portfolio = () => {
     return (
@@ -33,31 +36,51 @@ const Portfolio = () => {
                 <h3>Data Science <span>Fundamentals</span></h3>
                 <p>Possess foundational knowledge in data science, demonstrated through analyzing and deriving insights from the AOL search data set. Proficient in SQL for data querying, I have successfully investigated specific events and crafted relevant statistics, showcasing my ability to interpret and manipulate large data sets effectively.</p>
             </div>
-        <div className='stage-cube-cont'>
-            <div className='cubespinner'>
-                <div className='face1'>
-                <img src={html}/>
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <img src={html} alt=""/>
+                    </div>
+                    <div className='face2'>
+                        <img src={cpp} alt=""/>
+                    </div>
+                    <div className='face3'>
+                        <img src={css} alt=""/>
+                    </div>
+                    <div className='face4'>
+                        <img src={react} alt=""/>
+                    </div>
+                    <div className='face5'>
+                        <img src={java} alt=""/>
+                    </div>
+                    <div className='face6'>
+                        <img src={js} alt=""/>
+                    </div>
                 </div>
-                <div className='face2'>
-                <img src={cpp}/>
-                </div>
-                <div className='face3'>
-                <img src={css}/>
-                </div>
-                <div className='face4'>
-                <img src={react}/>
-                </div>
-                <div className='face5'>
-                <img src={java}/>
-                </div>
-                <div className='face6'>
-                <img src={js}/>
+                <div className='portfolio'>
+                    <div className='project'>
+                        <h2>Path-tracer in Java</h2>
+                        <a href="https://github.com/VerIPt/path-tracer">
+                            <img className='pathtracer' src={raytracer} alt="Path-tracer in Java" />
+                            <button className='view-project-btn'>View Project</button>
+                        </a>
+                    </div>
+                    <div className='project'>
+                    <h2>Treemap implemented in C++</h2>
+                        <a href="https://github.com/VerIPt/treemap">
+                        <img className='treemap' src={treemap} alt=""/>
+                            <button className='view-project-btn'>View Project</button>
+                        </a>
+                    </div>
+                    <div className='project'>
+                    <h2>Website The Louhaus Band</h2>
+                        <a href="https://www.louhausband.de/">
+                        <img className='louhaus' src={louhaus} alt=""/>
+                            <button className='view-project-btn'>View Project</button>
+                        </a>
+                    </div>
                 </div>
             </div>
-            <div className='pathtracer'>
-            <img src={react}/>
-            </div>
-        </div>
         </div>
     );
 }
