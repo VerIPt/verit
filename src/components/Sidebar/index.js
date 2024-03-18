e7e1230b33c8efbc46fe5445f159ed4f865cc4f9
s89799 <s89799>
2024-03-18 14:36:26 +0100

    sidebar responsive

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,48 +1,67 @@
 import { Link, NavLink } from 'react-router-dom'
+import { slide as Menu, handleOnClose } from 'react-burger-menu';
 import './index.scss'
 import LogoS from '../../assets/images/Logo_ohneService.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser, faEnvelope, faCode} from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope, faCode } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
-const Sidebar = () => (
+const Sidebar = () => {
+    
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+    
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
 
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt="logo" />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+        window.addEventListener('resize', handleResize);
 
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
-                <FontAwesomeIcon icon={faCode} color='#4d4d4e' />
+    return (
+        <Menu width={100} right={isMobile}>
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+                <img src={LogoS} alt="logo" />
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" className="home-link" to="/">
+                    <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
 
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
 
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/pius-maushart-9a81142a2/'>
-                <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
-                </a>
-            </li>
-            <li>
-                <a target='_blank' rel='noreferrer' href='https://github.com/VerIPt'>
-                <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
-                </a>
-            </li>
-        </ul>
-    </div>
-)
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
+                    <FontAwesomeIcon icon={faCode} color='#4d4d4e' />
+
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/pius-maushart-9a81142a2/'>
+                        <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://github.com/VerIPt'>
+                        <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </Menu>
+    );
+}
 
 export default Sidebar;
